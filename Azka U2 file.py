040c6459dda9430b4bb84ca01ec35a6c98725ec9
AzkaDar4297 <azka.dar@thekaustschool.org>
2025-04-23 15:05:34 +0300

    first code

---
+++ Azka U2 file.py
@@ -0,0 +1,4 @@
+#Message
+print("This quiz will test how much you know about countries around the world and the United Nations Sustainable Development Goals (SDGs)")
+print("There are 15 multiple-choice questions. For each question, choose the best answer: a, b, c, or d.")
+print("Good luck and have fun!")
